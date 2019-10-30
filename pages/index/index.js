378ec58a45873763bde97abc5d0a39210862dc1f
xiaozhuang <zhuangjinton@163.com>
2019-10-30 19:25:52 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,35 @@
-// pages/index/index.js
+//index.js
+import request from "../../utils/request.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    swiperImg:[],
+    navData:[],
+    floors:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad(){
+    request({
+      url:'/api/public/v1/home/swiperdata'
+    }).then(res=>{
+      const {message}=res.data;
+     this.setData({
+       swiperImg:message
+     })
+    });
+    request({
+      url:'/api/public/v1/home/catitems'
+    }).then(res=>{
+      const { message } = res.data;
+      this.setData({
+        navData: message
+      })
+    })
+    request({
+      url:"/api/public/v1/home/floordata"
+    }).then(res=>{
+      const {message}=res.data;
+      this.setData({
+        floors:message
+      })
+    })
   }
 })
